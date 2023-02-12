83b99780ab0e1f622f77fea97de356b67e2ce232
Jesmi-04 <jesmigeorge04@gmail.com>
2023-02-12 13:39:07 +0530

    Updated DLL

--- Linked List/DLL.py
+++ Linked List/DLL.py
@@ -47,6 +47,66 @@ class doublyLinkedList:
                 newNode.prev = tempNode
                 tempNode.next.prev = newNode
                 tempNode.next = newNode
+    
+    def traverseDLL(self):
+        if self.head is None:
+            print("The list is empty for traversal")
+        else:
+            node = self.head
+            while node:
+                print(node.value,end=',')
+                node = node.next
+            print()
+    
+    def reverseTraversalDLL(self):
+        if self.head is None:
+            print("The list is empty for traversal")
+        else:
+            node = self.tail
+            while node:
+                print(node.value,end=',')
+                node = node.prev
+            print()
+    
+    def searchDLL(self,key):
+        if self.head is None:
+            print("The list is empty for search")
+        else:
+            node = self.head
+            while node:
+                if node.value == key:
+                    print("Found")
+                    break
+                node = node.next
+            else:
+                print("Not Found")
+                
+    def deleteNode(self,location):
+        if self.head is None:
+            print("The list is empty for deletion")
+        else:
+            if location==0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = None
+            elif location==1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+            else:
+                curNode = self.head
+                index = 1
+                while index < location-1:
+                    curNode = curNode.next
+                    index+=1
+                curNode.next = curNode.next.next
+                curNode.next.prev = curNode 
                        
         
 dlst = doublyLinkedList()
@@ -59,4 +119,19 @@ dlst.insertNode(45,1)
 dlst.insertNode(72,1)
 print([node.value for node in dlst])
 dlst.insertNode(99,3)
+print([node.value for node in dlst])
+
+# to traverse a DLL
+dlst.traverseDLL()
+# to reverse traverse a DLL
+dlst.reverseTraversalDLL()
+#to search a DLL
+key = int(input("Enter value to search : "))
+dlst.searchDLL(key)
+
+#to delete a node from DLL
+dlst.deleteNode(0)
+dlst.deleteNode(1)
+print([node.value for node in dlst])
+dlst.deleteNode(2)
 print([node.value for node in dlst])
